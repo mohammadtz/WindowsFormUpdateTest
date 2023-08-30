c861c43ca6f6efb9bba54b2db3517b60e4ef7bf2
mohammadtz <mohammadtz1375@gmail.com>
2023-08-30 22:36:53 +0330

    update

--- WindowsFormUpdateTest/Form1.cs
+++ WindowsFormUpdateTest/Form1.cs
@@ -5,6 +5,8 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-        version.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+        version.Text = GetVersion();
     }
+
+    private string GetVersion() => $"Version: {Application.ProductVersion}";
 }
